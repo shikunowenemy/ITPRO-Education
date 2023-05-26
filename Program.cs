9b679676144da4377056d818fd10616820213d4c
Denis Shikunov <>
2023-05-26 21:40:24 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int studentsAmount = -1;
+int pagesAmount = -1;
+
+while (studentsAmount <= 0 || pagesAmount <= 0)
+{
+    Console.Write("Введите количество одногруппников: ");
+    studentsAmount = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество страниц для печати: ");
+    pagesAmount = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write($"Вам нужно распечатать {studentsAmount * pagesAmount} страниц");
