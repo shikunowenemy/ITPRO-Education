e6e67bb0d180c91d9bc30b2d5727a19fb26a4a54
Denis Shikunov <>
2023-06-01 20:32:50 +0300

    №2 ДЗ

--- Program.cs
+++ Program.cs
@@ -1,135 +1,89 @@
-﻿bool isCorrectValue; // Объявление переменной для проверки корректности введенного значения 
+﻿bool isCorrectValue;
+
 Console.WriteLine("------Задание 1------");
-double temperatureCelsius, temperatureFahrenheit, temperatureKelvin; // Объявление переменных температур
-while (true) // Бесконечный цикл выполнения программы, пока пользователь не введет правильное значение
+int startNumber, endNumber;
+
+while (true)
 {
-    Console.WriteLine("Введите температуру по шкале Цельсия: ");
-    isCorrectValue = double.TryParse(Console.ReadLine(), out temperatureCelsius); // Инициализация переменной проверки корректных данных
-    if (!isCorrectValue) // Если значение некорректно, то цикл начинается заново
+    Console.Write("Введите начальное число диапазона: ");
+    isCorrectValue = int.TryParse(Console.ReadLine(), out startNumber);
+
+    if (!isCorrectValue)
     {
-        Console.WriteLine("Вы ввели некорректные значения");
+        Console.WriteLine("Вы ввели некорретное значение");
         continue;
     }
-    temperatureFahrenheit = Math.Round(((temperatureCelsius / 9 * 5) + 32), 2); // Вычисление градусов по шкале Фаренгейта
-    temperatureKelvin = Math.Round(temperatureCelsius + 273.15, 2); // Вычисление градусов по шкале Кельвина
-    Console.WriteLine($"Температура по шкале Фаренгейта: {temperatureFahrenheit}\n" +
-        $"Температура по шкале Кельвина: {temperatureKelvin}");
-    break; // Завершение цикла
+
+    Console.Write("Введите конечное число диапазона: ");
+    isCorrectValue = int.TryParse(Console.ReadLine(), out endNumber);
+
+    if (!isCorrectValue || endNumber < startNumber)
+    {
+        Console.WriteLine("Вы ввели некорретное значение");
+        continue;
+    }
+
+    for (int i = startNumber; i <= endNumber; i++)
+    {
+        int reminder = i % 2;
+        if (reminder != 0)
+        {
+            Console.Write(i + " ");
+        }
+    }
+
+    break;
 }
 
 Console.WriteLine("------Задание 2------");
-double firstValue, secondValue;
 while (true)
 {
-    Console.WriteLine("Добро пожаловать в калькулятор введите операцию:\n" +
-    "+ --- сложение\n" +
-    "- --- вычитание\n" +
-    "* --- умножение\n" +
-    "/ --- деление\n" +
-    "% --- остаток от деления");
-    string mathOperation = Console.ReadLine();
-    Console.Write("Введите первое значение: ");
-    isCorrectValue = double.TryParse(Console.ReadLine(), out firstValue); // Инициализация переменной проверки корректных данных
-    if (!isCorrectValue) // Если значение некорректно, то цикл начинается заново
+    int userFibonacciLength;
+
+    Console.Write("Введите длину последовательности чисел Фибоначи: ");
+    isCorrectValue = int.TryParse(Console.ReadLine(), out userFibonacciLength);
+
+    if (!isCorrectValue || userFibonacciLength < 0)
     {
-        Console.WriteLine("Вы ввели некорректные значения");
+        Console.WriteLine("Вы ввели некорретное значение");
         continue;
-    };
-    Console.Write("Введите второе значение: ");
-    isCorrectValue = double.TryParse(Console.ReadLine(), out secondValue); // Инициализация переменной проверки корректных данных
-    if (!isCorrectValue) // Если значение некорректно, то цикл начинается заново
-    {
-        Console.WriteLine("Вы ввели некорректные значения");
-        continue;
-    };
-    switch (mathOperation)
-    {
-        case "+":
-            double sum = Math.Round(firstValue + secondValue, 2); // Вычисление суммы
-            Console.WriteLine($"{firstValue} + {secondValue} = {sum}");
-            break;
-        case "-":
-            double difference = Math.Round(firstValue - secondValue, 2);
-            Console.WriteLine($"{firstValue} - {secondValue} = {difference}"); // Вычисление разности
-            break;
-        case "*":
-            double product = Math.Round(firstValue * secondValue, 2);
-            Console.WriteLine($"{firstValue} * {secondValue} = {product}"); // Вычисление произведения
-            break;
-        case "/":
-            double quotient = Math.Round(firstValue / secondValue, 2);
-            Console.WriteLine($"{firstValue} / {secondValue} = {quotient}"); // Вычисление частного
-            break;
-        case "%":
-            double remainder = Math.Round(firstValue % secondValue, 2);
-            Console.WriteLine($"Остаток от деления {firstValue} на {secondValue} = {remainder}"); // Вычисление остатка от деления
-            break;
-        default: // Если операции не существует, то цикл запускается заново
-            Console.WriteLine("Некорректная операция");
-            continue;
     }
-    break; // Завершение цикла
+
+    switch (userFibonacciLength)
+    {
+        case 0:
+            Console.WriteLine(0);
+            break;
+        case 1:
+            Console.WriteLine(0 + " " + 1);
+            break;
+        default:
+            int[] fibonacciNumbers = new int[userFibonacciLength];
+            fibonacciNumbers[0] = 0;
+            fibonacciNumbers[1] = 1;
+
+            for (int i = 2; i < fibonacciNumbers.Length; i++)
+            {
+                fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+            }
+
+            for (int i = 0; i < fibonacciNumbers.Length; i++)
+            {
+                Console.Write(fibonacciNumbers[i] + " ");
+            }
+            break;
+    }
+    break;
 }
+
 Console.WriteLine("------Задание 3------");
-Console.WriteLine("Проверим ваши знания на таблицу умножения!");
-while (true)
+
+int day = 1;
+double userDistance = 10, stepDistance = 10, targetDistance = 100, sumDistance = 0;
+while (sumDistance < targetDistance)
 {
-    int firstNumber, secondNumber, userAnswer, productValues; // Объявление множимого, множителя, ответа пользователя и произведения чисел
-    Console.Write("Введите первое число: ");
-    isCorrectValue = int.TryParse(Console.ReadLine(), out firstNumber); // Инициализация переменной проверки корректных данных
-    if (!isCorrectValue) // Если значение некорректно, то цикл начинается заново
-    {
-        Console.WriteLine("Вы ввели некорректное значение");
-        continue;
-    };
-    Console.Write("Введите второе число: ");
-    isCorrectValue = int.TryParse(Console.ReadLine(), out secondNumber); // Инициализация переменной проверки корректных данных
-    if (!isCorrectValue) // Если значение некорректно, то цикл начинается заново
-    {
-        Console.WriteLine("Вы ввели некорректное значение");
-        continue;
-    };
-    productValues = firstNumber * secondNumber;
-    Console.Write("Введите свой ответ: ");
-    isCorrectValue = int.TryParse(Console.ReadLine(), out userAnswer); // Инициализация переменной проверки корректных данных
-    if (!isCorrectValue) // Если значение некорректно, то цикл начинается заново
-    {
-        Console.WriteLine("Вы ввели некорректное значение");
-        continue;
-    };
-    if (userAnswer == productValues)
-    {
-        Console.WriteLine("Да, ответ правильный!");
-    }
-    else
-    {
-        Console.WriteLine($"Вы ошиблись! Правильный ответ {productValues}");
-    }
-    break;
+    sumDistance += userDistance;
+    userDistance = userDistance / 100 * (100 + stepDistance);
+    day++;
 }
-Console.WriteLine("------Задание 4------");
-while (true)
-{
-    int n;
-    Console.Write("Введите количество лет: ");
-    isCorrectValue = int.TryParse(Console.ReadLine(), out n); // Инициализация переменной проверки корректных данных
-    if (!isCorrectValue || n < 1 || n > 99) // Если значение некорректно, то цикл начинается заново
-    {
-        Console.WriteLine("Вы ввели некорректное значение");
-        continue;
-    };
-    int remainder = n % 10; // Инициализация переменой, отбрасывающая десятки
-    if (remainder == 0 || remainder >= 5)
-    {
-        Console.WriteLine($"Мне {n} лет");
-    }
-    else if (remainder == 1) 
-    {
-        Console.WriteLine($"Мне {n} год");
-    }
-    else
-    {
-        Console.WriteLine($"Мне {n} года");
-    }
-    break;
-}
+
